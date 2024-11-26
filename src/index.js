83e17a787933d97d86739b1dcc2c5c8f2f189773
unknown <krithikroshan113@gmail.com>
2024-11-26 11:42:37 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,14 +7,7 @@ import axios from 'axios';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 axios.defaults.baseURL = "https://todos-backend-jchp.onrender.com";
 root.render(
-  <React.StrictMode>
     <InputsContext>
       <App />
     </InputsContext>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+)
